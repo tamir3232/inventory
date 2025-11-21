200bb0152bc03ba0f7c251b3cdcb809497f4d4f2
th21805x <tamir.hega@cimbniaga.co.id>
2025-11-21 23:16:59 +0700

    fixed the path warehouse

--- inventory-backend/src/routes/warehouseRoute.js
+++ inventory-backend/src/routes/warehouseRoute.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/WarehouseController");
+const controller = require("../controllers/warehouseController");
 
 router.get("/", controller.getWarehouse);
 
